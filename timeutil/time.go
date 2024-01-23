84a904f9f3df5b11b5c0c985e613fb2df3e4beb2
oldjon <157625550@qq.com>
2024-01-23 19:18:28 +0800

    [feature] time util

--- timeutil/time.go
+++ timeutil/time.go
@@ -4,93 +4,87 @@ import (
 	"time"
 )
 
-// GetTimeUnix using local time zone
-func GetTimeUnix(t time.Time) int64 {
-	return t.Local().Unix()
+// GetNowTimeStr string format "2006-01-02 15:04:05" if loc is nil, using local time zone
+func GetNowTimeStr(loc *time.Location) string {
+	if loc == nil {
+		loc = time.Local
+	}
+	return TimeToDateTime(time.Now().In(loc))
 }
 
-// GetNowTimeUnix using local time zone
-func GetNowTimeUnix() int64 {
-	return GetTimeUnix(time.Now())
+// GetNowDateStr string format "2006-01-02" if loc is nil, using local time zone
+func GetNowDateStr(loc *time.Location) string {
+	if loc == nil {
+		loc = time.Local
+	}
+	return TimeToDate(time.Now().In(loc))
 }
 
-// GetNowTimeStr string format "2006-01-02 15:04:05"
-func GetNowTimeStr() string {
-	return TimeToDateTime(time.Now())
-}
-
-// GetNowDateStr string format "2006-01-02"
-func GetNowDateStr() string {
-	return TimeToDate(time.Now())
-}
-
-// GetTimeFromUnix using local time zone
+// GetTimeFromUnix
 func GetTimeFromUnix(timestamp int64) time.Time {
 	return time.Unix(timestamp, 0)
 }
 
-// TodayXHourTime using local time zone
-func TodayXHourTime(addHour time.Duration) time.Time {
-	dateStr := TimeToDate(time.Now())
-	t := DateStrToTime(dateStr)
-	return t.Add(time.Hour * addHour)
-}
-
-// TodayXHourTimeUnix using local time zone
-func TodayXHourTimeUnix(addHour time.Duration) int64 {
-	dateStr := TimeToDate(time.Now())
-	t := DateStrToTime(dateStr)
-	t2 := t.Add(time.Hour * addHour)
-	return GetTimeUnix(t2)
-}
-
-// TomorrowXHourTime using local time zone
-func TomorrowXHourTime(addHour time.Duration) time.Time {
-	dateStr := TimeToDate(time.Now())
-	t := DateStrToTime(dateStr)
-	return t.Add(time.Hour * (24 + addHour))
-}
-
-// TomorrowXHourTimeByLocation using local time zone
-func TomorrowXHourTimeByLocation(addHour time.Duration, loc *time.Location) time.Time {
+// TodayXHourTime if loc is nil, using local time zone
+func TodayXHourTime(addHour time.Duration, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.Local
+	}
 	dateStr := TimeToDate(time.Now().In(loc))
-	t := DateStrToTimeByLocation(dateStr, loc)
-	return t.Add(time.Hour * (24 + addHour))
+	return DateStrToTime(dateStr, loc).Add(time.Hour * addHour)
 }
 
-// TodayXHourTimeByLocation using local time zone
-func TodayXHourTimeByLocation(addHour time.Duration, loc *time.Location) time.Time {
+// TodayXHourTimeUnix if loc is nil, using local time zone
+func TodayXHourTimeUnix(addHour time.Duration, loc *time.Location) int64 {
+	if loc == nil {
+		loc = time.Local
+	}
 	dateStr := TimeToDate(time.Now().In(loc))
-	t := DateStrToTimeByLocation(dateStr, loc)
-	return t.Add(time.Hour * addHour)
+	return DateStrToTime(dateStr, loc).Add(time.Hour * addHour).Unix()
 }
 
-// TomorrowXHourTimeUnix using local time zone
-func TomorrowXHourTimeUnix(addHour time.Duration) int64 {
-	dateStr := TimeToDate(time.Now())
-	t := DateStrToTime(dateStr)
-	t2 := t.Add(time.Hour * (24 + addHour))
-	return GetTimeUnix(t2)
+// TomorrowXHourTime if loc is nil, using local time zone
+func TomorrowXHourTime(addHour time.Duration, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.Local
+	}
+	dateStr := TimeToDate(time.Now().In(loc))
+	return DateStrToTime(dateStr, loc).Add(time.Hour * (24 + addHour))
 }
 
-// LastXHourTime using local time zone
-func LastXHourTime(addHour time.Duration) time.Time {
-	dateStr := TimeToDate(time.Now().Add(-time.Hour * 24))
-	t := DateStrToTime(dateStr)
-	return t.Add(time.Hour * addHour)
+// TomorrowXHourTimeUnix if loc is nil, using local time zone
+func TomorrowXHourTimeUnix(addHour time.Duration, loc *time.Location) int64 {
+	if loc == nil {
+		loc = time.Local
+	}
+	dateStr := TimeToDate(time.Now().In(loc))
+	return DateStrToTime(dateStr, loc).Add(time.Hour * (24 + addHour)).Unix()
 }
 
-// LastXHourTimeUnix using local time zone
-func LastXHourTimeUnix(addHour time.Duration) int64 {
-	dateStr := TimeToDate(time.Now().Add(-time.Hour * 24))
-	t := DateStrToTime(dateStr)
-	t2 := t.Add(time.Hour * addHour)
-	return GetTimeUnix(t2)
+// LastXHourTime if loc is nil, using local time zone
+func LastXHourTime(addHour time.Duration, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.Local
+	}
+	dateStr := TimeToDate(time.Now().In(loc).Add(-time.Hour * 24))
+	return DateStrToTime(dateStr, loc).Add(time.Hour * addHour)
 }
 
-// ThisMondayXHourTimeUnix using local time zone
-func ThisMondayXHourTimeUnix(addHour time.Duration) int64 {
-	now := time.Now()
+// LastXHourTimeUnix if loc is nil, using local time zone
+func LastXHourTimeUnix(addHour time.Duration, loc *time.Location) int64 {
+	if loc == nil {
+		loc = time.Local
+	}
+	dateStr := TimeToDate(time.Now().In(loc).Add(-time.Hour * 24))
+	return DateStrToTime(dateStr, loc).Add(time.Hour * addHour).Unix()
+}
+
+// ThisMondayXHourTimeUnix if loc is nil, using local time zone
+func ThisMondayXHourTimeUnix(addHour time.Duration, loc *time.Location) int64 {
+	if loc == nil {
+		loc = time.Local
+	}
+	now := time.Now().In(loc)
 	weekDay := now.Weekday()
 	if weekDay == time.Sunday {
 		now = now.AddDate(0, 0, -6)
@@ -98,21 +92,25 @@ func ThisMondayXHourTimeUnix(addHour time.Duration) int64 {
 		now = now.AddDate(0, 0, -int(weekDay)+1)
 	}
 	dateStr := TimeToDate(now)
-	t := DateStrToTime(dateStr)
-	t2 := t.Add(time.Hour * addHour)
-	return GetTimeUnix(t2)
+	return DateStrToTime(dateStr, loc).Add(time.Hour * addHour).Unix()
 }
 
-// AddDateToTime add year,month and date
-func AddDateToTime(t int64, addYear int, addMonth int, addDate int) int64 {
-	ti := GetTimeFromUnix(t)
+// AddDateToTime add year,month and date, if loc is nil, using local time zone
+func AddDateToTime(t int64, addYear int, addMonth int, addDate int, loc *time.Location) int64 {
+	if loc == nil {
+		loc = time.Local
+	}
+	ti := GetTimeFromUnix(t).In(loc)
 	ti = ti.AddDate(addYear, addMonth, addDate)
-	return GetTimeUnix(ti)
+	return ti.Unix()
 }
 
-// AddSecondToNowTime add seconds to now time
-func AddSecondToNowTime(addSecs int) string {
-	now := time.Now()
+// AddSecondToNowTime add seconds to now time, if loc is nil, using local time zone
+func AddSecondToNowTime(addSecs int, loc *time.Location) string {
+	if loc == nil {
+		loc = time.Local
+	}
+	now := time.Now().In(loc)
 	newTime := now.Add(time.Second * time.Duration(addSecs))
 	return TimeToDateTime(newTime)
 }
@@ -128,64 +126,56 @@ func TimeToDateTime(t time.Time) string {
 }
 
 // DateStrToTime string format "2006-01-02"
-func DateStrToTime(d string) time.Time {
-	t, _ := time.ParseInLocation("2006-01-02", d, time.Local)
-	return t
-}
-
-// DateStrToTimeByLocation string format "2006-01-02"
-func DateStrToTimeByLocation(d string, loc *time.Location) time.Time {
+func DateStrToTime(d string, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.Local
+	}
 	t, _ := time.ParseInLocation("2006-01-02", d, loc)
 	return t
 }
 
 // DateStrToTimeUnix string format "2006-01-02" to timestamp
-func DateStrToTimeUnix(d string) int64 {
+func DateStrToTimeUnix(d string, loc *time.Location) int64 {
 	if d == "" {
 		return 0
 	}
-
-	t, _ := time.ParseInLocation("2006-01-02", d, time.Local)
+	if loc == nil {
+		loc = time.Local
+	}
+	t, _ := time.ParseInLocation("2006-01-02", d, loc)
 	return t.Unix()
 }
 
 // DateTimeStrToTime string format "2006-01-02 15:04:05"
-func DateTimeStrToTime(dt string, locationStr string) time.Time {
-	var location = time.Local
-	if locationStr != "" {
-		location, _ = time.LoadLocation(locationStr)
+func DateTimeStrToTime(dt string, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.Local
 	}
-	t, _ := time.ParseInLocation("2006-01-02 15:04:05", dt, location)
+	t, _ := time.ParseInLocation("2006-01-02 15:04:05", dt, loc)
 	return t
 }
 
 // DateTimeStrToTimeUnix string format "2006-01-02 15:04:05" to timestamp
-func DateTimeStrToTimeUnix(dt string, locationStr string) int64 {
+func DateTimeStrToTimeUnix(dt string, loc *time.Location) int64 {
 	if dt == "" {
 		return 0
 	}
-	var location = time.Local
-	if locationStr != "" {
-		location, _ = time.LoadLocation(locationStr)
+	if loc == nil {
+		loc = time.Local
 	}
-	t, _ := time.ParseInLocation("2006-01-02 15:04:05", dt, location)
+	t, _ := time.ParseInLocation("2006-01-02 15:04:05", dt, loc)
 	return t.Unix()
 }
 
 // DateTimeStrToTimeUnix string format "2006-01-02 15:04:05" to timestamp
-func DateTimeStrToTimeUnixWithErr(dt string, locationStr string) (int64, error) {
+func DateTimeStrToTimeUnixWithErr(dt string, loc *time.Location) (int64, error) {
 	if dt == "" {
 		return 0, nil
 	}
-	var location = time.Local
-	var err error
-	if locationStr != "" {
-		location, err = time.LoadLocation(locationStr)
-		if err != nil {
-			return 0, err
-		}
+	if loc == nil {
+		loc = time.Local
 	}
-	t, err := time.ParseInLocation("2006-01-02 15:04:05", dt, location)
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", dt, loc)
 	if err != nil {
 		return 0, err
 	}
@@ -205,22 +195,22 @@ func IsWeekend(t time.Time) bool {
 // For exmaple, if the refresh time is Monday 08:00 in each region and check region is -0500 EST
 // If t is 2018-04-02 12:59 0000 UTC which is 2018-04-02 07:59 -0500 EST would return 2018-03-26 08:00 -0500 EST
 // If t is 2018-04-02 13:01 0000 UTC which is 2018-04-02 08:01 -0500 EST would return 2018-04-02 08:00 -0500 EST
-func LastRegionWeekRefreshTime(t time.Time, refreshDay time.Weekday, refreshHour int, refreshMinutes int, region *time.Location) time.Time {
-	tt := t.In(region)
+func LastRegionWeekRefreshTime(t time.Time, refreshDay time.Weekday, refreshHour int, refreshMinutes int, loc *time.Location) time.Time {
+	tt := t.In(loc)
 	weekday := tt.Weekday()
 	year, month, day := tt.Date()
 	hour, minute, _ := tt.Clock()
 	if weekday < refreshDay ||
 		(refreshDay == weekday && hour < refreshHour) ||
 		(refreshDay == weekday && hour == refreshHour && minute <= refreshMinutes) {
-		return time.Date(year, month, day+int(refreshDay-weekday)-7, refreshHour, refreshMinutes, 0, 0, region)
+		return time.Date(year, month, day+int(refreshDay-weekday)-7, refreshHour, refreshMinutes, 0, 0, loc)
 	}
-	return time.Date(year, month, day-int(weekday-refreshDay), refreshHour, refreshMinutes, 0, 0, region)
+	return time.Date(year, month, day-int(weekday-refreshDay), refreshHour, refreshMinutes, 0, 0, loc)
 }
 
 // RegionMonday5HourDateTime return region Monday 05am date, format: 2006-01-02 04:00:00
-func RegionMonday4HourDateTime(region *time.Location) string {
-	now := time.Now().In(region)
+func RegionMonday4HourDateTime(loc *time.Location) string {
+	now := time.Now().In(loc)
 	weekDay := now.Weekday()
 	if weekDay == time.Sunday {
 		now = now.AddDate(0, 0, -6)
