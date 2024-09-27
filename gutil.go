2404d7524b86c71daf341553df24d92d0a3eabc4
oldjon <157625550@qq.com>
2024-09-27 17:13:23 +0800

    [feature] update go.mod

--- gutil.go
+++ gutil.go
@@ -3,9 +3,6 @@ package gutil
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"strconv"
-	"strings"
-
 	"golang.org/x/exp/constraints"
 )
 
@@ -46,58 +43,4 @@ func Bound[T constraints.Ordered](a, min, max T) T {
 		return max
 	}
 	return a
-}
-
-func StrToUint32(str string) uint32 {
-	n, _ := strconv.ParseUint(str, 10, 32)
-	return uint32(n)
-}
-
-func StrToUint64(str string) uint64 {
-	n, _ := strconv.ParseUint(str, 10, 64)
-	return n
-}
-
-func StrToInt32(str string) int32 {
-	n, _ := strconv.ParseInt(str, 10, 32)
-	return int32(n)
-}
-
-func StrToInt64(str string) int64 {
-	n, _ := strconv.ParseInt(str, 10, 64)
-	return n
-}
-
-func StrToFloat32(str string) float32 {
-	n, _ := strconv.ParseFloat(str, 32)
-	return float32(n)
-}
-
-func StrToFloat64(str string) float64 {
-	n, _ := strconv.ParseFloat(str, 64)
-	return n
-}
-
-func StrToBool(str string) bool {
-	str = strings.ToLower(str)
-	if str == "true" || str == "1" {
-		return true
-	}
-	return false
-}
-
-func Uint32ToString(i uint32) string {
-	return strconv.FormatUint(uint64(i), 10)
-}
-
-func Uint64ToString(i uint64) string {
-	return strconv.FormatUint(i, 10)
-}
-
-func Int32ToString(i int32) string {
-	return strconv.FormatInt(int64(i), 10)
-}
-
-func Int64ToString(i int64) string {
-	return strconv.FormatInt(i, 10)
 }
